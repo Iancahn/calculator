ca2d7fee6b68aa517a6cdf119b29b8d7d4d3648f
Ian Cahn <iancahn@live.com>
2022-10-12 23:41:06 +0200

    Calculator buttons update
    Added calculator and clear button

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello World!")
+console.log("Hello World!");
+
+let calculations = 2;
+document.getElementById("calculations").innerHTML = `${calculations}`;
+
+function clearField() {
+    let calculations = 0;
+    document.getElementById("calculations").innerHTML = `${calculations}`;
+    console.log("I was clicked");
+}
